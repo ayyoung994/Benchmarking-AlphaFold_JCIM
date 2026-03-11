99b97c84336a25a26647b629e0cbbb6c0cc463c2
ayyoung994 <aykarunia06@gmail.com>
2026-03-11 12:00:29 +0900

    Update 01_rmsd_ca.sh

--- analysis/01_5FWJ_holo/01_global_stability_analysis/scripts/01_rmsd_ca.sh
+++ analysis/01_5FWJ_holo/01_global_stability_analysis/scripts/01_rmsd_ca.sh
@@ -2,9 +2,10 @@
 # System: 01_5FWJ_holo
 # Fit group: 27 (JmjC_ChainB_CA)
 # RMSD group: 27 (JmjC_ChainB_CA)
+# Replace repX with rep1, rep2, or rep3 before running.
 
-gmx rms -s md_rep2_60_protein.tpr \
+gmx rms -s md_repX_60_protein.tpr \
   -f md_0_60_fit_core.xtc \
   -n jmjc.ndx \
-  -o rmsd_jmjc_CA_5fwj_holo_60_rep2.xvg \
+  -o rmsd_jmjc_CA_holo_60_repX.xvg \
   -tu ns
