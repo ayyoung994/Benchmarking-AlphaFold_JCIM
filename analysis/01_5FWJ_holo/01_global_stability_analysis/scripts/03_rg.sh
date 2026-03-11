8f004bb69afd82fe12ebfb9d345d6218d4c72b07
ayyoung994 <aykarunia06@gmail.com>
2026-03-11 12:02:57 +0900

    Create 03_rg.sh

---
+++ analysis/01_5FWJ_holo/01_global_stability_analysis/scripts/03_rg.sh
@@ -0,0 +1,10 @@
+# Analysis: Radius of gyration
+# System: 01_5FWJ_holo
+# Group: 26 (JmjC_ChainB)
+# Replace repX with rep1, rep2, or rep3 before running.
+
+gmx gyrate -s md_repX_60_protein.tpr \
+  -f md_0_60_center.xtc \
+  -n jmjc.ndx \
+  -o rg_jmjc_EXPB_60_repX.xvg \
+  -tu ns
