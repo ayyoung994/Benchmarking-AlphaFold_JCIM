b95de21afc0fdd1a9cd3bd2eb2205dca29a4fcc4
ayyoung994 <aykarunia06@gmail.com>
2026-03-13 11:31:38 +0900

    Update run_mmgbsa_rep1.sh

--- MMGBSA/01_5FWJ_holo/scripts/run_mmgbsa_rep1.sh
+++ MMGBSA/01_5FWJ_holo/scripts/run_mmgbsa_rep1.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+set -e
 
 # MM/GBSA workflow for 01_5FWJ_holo, replica 1
 
 # Step 1: prepare MM/GBSA index file
-gmx make_ndx -f md_rep1.tpr -n jmjc.ndx -o jmjc_mmpbsa.ndx
+gmx make_ndx -f md_rep1.tpr -n jmjc.ndx -o ../input/jmjc_mmpbsa.ndx
 
 # Interactive commands used inside make_ndx:
 # 26 | 14 | 15 | 16
@@ -20,13 +21,15 @@ gmx trjconv -s md_rep1.tpr -f md_rep1.xtc -o md_0_60_center_2.xtc -b 0 -e 60000 
 # Step 3: run MM/GBSA
 # receptor group: 36 (Receptor_JmjC_B_holo)
 # ligand group: 17 (DOL)
+# shared input file: ../input/mmpbsa.in
+# shared index file: ../input/jmjc_mmpbsa.ndx
 
 gmx_MMPBSA -O \
-  -i mmpbsa.in \
+  -i ../input/mmpbsa.in \
   -cs md_rep1.tpr \
   -ct md_0_60_center_2.xtc \
   -cp topol.top \
-  -ci jmjc_mmpbsa.ndx \
+  -ci ../input/jmjc_mmpbsa.ndx \
   -cg 36 17 \
   -o FINAL_RESULTS_MMPBSA.dat \
   -eo FINAL_RESULTS_PER_FRAME.dat \
