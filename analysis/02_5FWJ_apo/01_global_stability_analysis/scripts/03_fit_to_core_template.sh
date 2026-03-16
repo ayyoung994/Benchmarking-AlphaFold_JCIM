4f79b007af6af60414f15710df76c108fb98606d
ayyoung994 <aykarunia06@gmail.com>
2026-03-16 15:56:37 +0900

    Create 03_fit_to_core_template.sh

---
+++ analysis/02_5FWJ_apo/01_global_stability_analysis/scripts/03_fit_to_core_template.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+# Template script for fitting the trajectory to the JmjC core C-alpha atoms
+# for downstream global stability analysis.
+#
+# Replace `md_repX.tpr` with the replica-specific TPR filename.
+# This script assumes that `md_0_60_center.xtc` and `jmjc.ndx` have already been prepared.
+
+gmx trjconv \
+  -s md_repX.tpr \
+  -f md_0_60_center.xtc \
+  -o md_0_60_fit.xtc \
+  -fit rot+trans \
+  -n jmjc.ndx
+
+# Interactive selections:
+# Least-squares fit group: JmjC_ChainB_CA (24)
+# Output group: Protein (1)
