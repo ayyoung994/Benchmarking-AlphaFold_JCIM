588b9e71b67075b2ce459896ef3a69841e071827
ayyoung994 <aykarunia06@gmail.com>
2026-03-17 13:55:12 +0900

    Update 01_make_DOL_heavy_index.sh

--- analysis/02_5FWJ_apo/03_ligand_behavior/scripts/01_make_DOL_heavy_index.sh
+++ analysis/02_5FWJ_apo/03_ligand_behavior/scripts/01_make_DOL_heavy_index.sh
@@ -7,10 +7,10 @@ set -e
 # - The ligand group DOL is group 13 in jmjc.ndx.
 # - The newly created heavy-atom group is temporarily assigned as group 34
 #   before being renamed to DOL_heavy.
-# - Replace `md_repX_60.tpr` with the replica-specific TPR filename before use.
+# - Replace `md_repX.tpr` with the replica-specific TPR filename before use.
 
 gmx make_ndx \
-  -f md_repX_60.tpr \
+  -f md_repX.tpr \
   -n jmjc.ndx \
   -o jmjc.ndx << 'EOF'
 13 & ! a H*
